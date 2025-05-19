13843e1fa25f74edb6a883f3e0f34c4bfbee3358
mahii060 <mdmahebul-27-2020518110@ier.du.ac.bd>
2025-05-19 16:55:23 +0600

    Query learned

--- src/pages/shared/Navbar.jsx
+++ src/pages/shared/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <li><a>Item 3</a></li>
     </>
     return (
-        <div className="navbar bg-blue-50 rounded">
+        <div className="navbar bg-blue-50 rounded ">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
